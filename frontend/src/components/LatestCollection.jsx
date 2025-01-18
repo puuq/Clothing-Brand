5d8ef39644de2527994bef721520e46473643b97
San Choo <littlecono1@gmail.com>
2025-01-18 15:06:27 +0545

    latest collection done

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,25 +1,38 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 
 const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-    const { products } = useContext(ShopContext);
-    const {latestProducts,setLatestProducts} = useState([]);
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, [products]);
 
-    useEffect(() => {
-        setLatestProducts(products.slice(0,10));
-    }, [])
-    
   return (
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-            <Title text1={'LATEST'} text2={'COLLECTION'}/>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+        </p>
+      </div>
+      
+        {/* Rendering Products  */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+          {latestProducts.map((product, index) => (
+            <div key={index} className='p-4 border rounded shadow'>
+              <h3 className='text-xl font-semibold'>{product.name}</h3>
+              <p className='text-gray-600'>{product.description}</p>
+            </div>
+          ))}
         </div>
-    </div>
-  )
-}
 
-export default LatestCollection
+    </div>
+  );
+};
+
+export default LatestCollection;
---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { Link } from 'react-router-dom'
+
+const ProductItem = (id,image,name,price) => {
+
+    const {currency} = useContext(ShopContext)
+
+  return (
+    <link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+        <div className='overflow-hidden'>
+            <img className='hover:scale-110 transition ease-in-out' src="{image[0]}" alt="" />
+        </div>
+        <p className='pt-3 pb-1 text-sm'>{name}</p>
+        <p className='text-sm font-medium'>{currency}{price}</p>
+    </link>
+  )
+}
+
+export default ProductItem
